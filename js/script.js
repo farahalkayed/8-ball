780d46c3ef31cf3b19475e2af6e293e738c646a2
farahalkayed <97057392+farahalkayed@users.noreply.github.com>
2022-03-07 10:05:11 -0800

    update

--- js/script.js
+++ js/script.js
@@ -40,18 +40,6 @@ switch (expr) {
 }
 })
 
-$("#button").click(function () {
-  var tayAudio = $("#taytay")[0]
-  if (tayAudio.paused == true) {
-    $("#taytay")[0].play()
-    $("#taylor-button").html("Pause Taylor")
-
-    //$("audio#pop")[0].currentTime = 0
-  } else {
-    $("#taytay")[0].pause()
-    $("#taylor-button").html("Play Taylor")
-  }
-})
 
 
 
